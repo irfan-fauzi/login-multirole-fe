4879459f0e65c2da08a5cf04de5f9a171161e890
irfan-fauzi <irfanwintech@yahoo.com>
2024-06-27 22:51:18 +0700

    create sidebar

---
+++ src/components/layouts/AuthLayout.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react'
+import Header from './Header'
+import Sidebar from './Sidebar'
+
+
+const AuthLayout:FC = () => {
+  return (
+    <div className='flex'></div>
+  )
+}
+
+export default AuthLayout
